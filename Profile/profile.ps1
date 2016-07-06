a48ef31da443a40ecd26a2ace9a256ff121094f5
Brian Sheaffer <brsh@users.noreply.github.com>
2016-07-06 14:15:00 -0700

    Separated Functions from init scripts
    Renamed private to functions so as to better describe what's in there.
    It also allows me to separate out scripts that might rely on functions -
    functions load first, then init scripts can utilize them.

--- Profile/profile.ps1
+++ Profile/profile.ps1
@@ -26,9 +26,14 @@ if(!$global:WindowTitlePrefix) {
 
 ################### Inits ######################
 
-# Dot sourcing private script files
-if (Test-Path $Global:LibPath\Profile\private) {
-    Get-ChildItem $Global:LibPath\Profile\private -Recurse -Filter "*.ps1" -File | Foreach { 
+# Dot sourcing private function and script files
+if (Test-Path $Global:LibPath\Profile\functions) {
+    Get-ChildItem $Global:LibPath\Profile\functions -Recurse -Filter "*.ps1" -File | Foreach { 
+        . $_.FullName
+    }
+}
+if (Test-Path $Global:LibPath\Profile\scripts) {
+    Get-ChildItem $Global:LibPath\Profile\scripts -Recurse -Filter "*.ps1" -File | Foreach { 
         . $_.FullName
     }
 }
@@ -72,10 +77,6 @@ Set-PSReadLineOption –HistoryNoDuplicates:$True
 
 #Modules
 if (test-path $Global:LibPath\Modules) {
-    #I recommend:
-    #   changing the owner of the folder to "Administrators" or "Administrator"
-    #   and removing read/write from general users (like 'Authenticated Users' or 'Everyone'... or your own account)
-    #Just to restrict changes a little - just a little...
     Get-ChildItem $Global:LibPath\Modules *.psm1 -Recurse | ForEach-Object { Import-Module $_.FullName -force }
 }
 
@@ -86,35 +87,6 @@ if (test-path $Global:LibPath\Settings\psdrive.csv) {
 
 if (Get-Service VMTools -ea SilentlyContinue) {
     New-ProfilePSDrive -name VMHost -Location "\\vmware-host\Shared Folders\$env:username\scripts" -Description "VMHost scripts"
-}
-
-## GitHub
-if ((Test-Path $env:LOCALAPPDATA\GitHub\shell.ps1) -and ($env:github_git -eq $null)) { 
-    #Ok, we have GitHub - let's make a profile psdrive
-    New-ProfilePSDrive -name GitHome -Location $env:LOCALAPPDATA\GitHub -Description "Git program and source files"
-    #Now I'll parse github's shell.ps1 - pulling out only the code I want
-    ##   Anything that sets and environment variable (match $env at the start of a line)
-    ##       But not the editor (notmatch EDITOR)
-    ##       But not posh_git (notmach posh_git)
-    ##   And add the variables needed for the Path statment (match $pGitPath, $appPath, $msBuildPath)
-    #and run it as a script expression
-    [String]$ShellCodeToRun = ""
-    Get-Content $env:LOCALAPPDATA\GitHub\shell.ps1 | Where-Object { 
-        (
-            (($_.ToString().Trim() -match "^.env") `
-            -and ($_ -notmatch "EDITOR") `
-            -and ($_ -notmatch "posh_git")) `
-            -or ($_.ToString().Trim() -match "^.pGitPath|^.appPath|^.msBuildPath") 
-        )
-    } | ForEach-Object { $ShellCodeToRun += $_ + ";" }
-    if ($ShellCodeToRun.Length -gt 0) { 
-        Try {
-            Invoke-Expression $ShellCodeToRun 
-        }
-        Catch {
-            write-host "Could not initialize Git!" -ForegroundColor Red -BackgroundColor Black
-        }
-    }
 }
 
 #Path Adjustments
---
+++ Profile/scripts/load-github.ps1
@@ -0,0 +1,28 @@
+﻿## GitHub
+if ((Test-Path $env:LOCALAPPDATA\GitHub\shell.ps1) -and ($env:github_git -eq $null)) { 
+    #Ok, we have GitHub - let's make a profile psdrive
+    New-ProfilePSDrive -name GitHome -Location $env:LOCALAPPDATA\GitHub -Description "Git program and source files"
+    #Now I'll parse github's shell.ps1 - pulling out only the code I want
+    ##   Anything that sets and environment variable (match $env at the start of a line)
+    ##       But not the editor (notmatch EDITOR)
+    ##       But not posh_git (notmach posh_git)
+    ##   And add the variables needed for the Path statment (match $pGitPath, $appPath, $msBuildPath)
+    #and run it as a script expression
+    [String]$ShellCodeToRun = ""
+    Get-Content $env:LOCALAPPDATA\GitHub\shell.ps1 | Where-Object { 
+        (
+            (($_.ToString().Trim() -match "^.env") `
+            -and ($_ -notmatch "EDITOR") `
+            -and ($_ -notmatch "posh_git")) `
+            -or ($_.ToString().Trim() -match "^.pGitPath|^.appPath|^.msBuildPath") 
+        )
+    } | ForEach-Object { $ShellCodeToRun += $_ + ";" }
+    if ($ShellCodeToRun.Length -gt 0) { 
+        Try {
+            Invoke-Expression $ShellCodeToRun 
+        }
+        Catch {
+            write-host "Could not initialize Git!" -ForegroundColor Red -BackgroundColor Black
+        }
+    }
+}
