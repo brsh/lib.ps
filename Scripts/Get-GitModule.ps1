36f81b429628d04abbc4690d42d25af0bab50ab1
Brian Sheaffer <brsh@users.noreply.github.com>
2016-07-06 09:52:23 -0700

    Simplifying modules add
    I wrote a script to bring my "standard" modules down.  Still need to do
    some automation of the folder structure....
    
    Also, added a readme with installation instructions.

---
+++ Scripts/Get-GitModule.ps1
@@ -0,0 +1,97 @@
+﻿<# 
+.SYNOPSIS 
+    Gets a PS Module from GitHub
+
+.DESCRIPTION 
+    Intended as a simple way to "build" my PowerShell library's Module folder. Provided the github url (well, git url, really), this script will clone the module into the folder specifed (C:\Scripts\Modules by default). The module can be "read-only" - meaning it will only include 1 level of history (i.e., the current version) - or can be a full, normal clone.
+
+    By default, this script will not update anything if the folder exists. I don't intend the library modules to be "live" clones, so the standard action if the module has already been cloned is to do Nothing. If you want to download a new copy, you must specify -Force, which will delete the module files and then re-download them.
+    
+    In my lib configuration (until I splurge on a code signing cert, anyway), I've adjusted permissions so this script must be run as admin.
+
+    Generally, unless there are problems, this is a silent script - use -Verbose to make it noisier.
+
+.PARAMETER URL
+    Path to the git repository - any path supported by git should work (although I've only tested https)
+
+.PARAMETER DestPath
+    The path where the module will copied. This is the parent path, with the name of the module (seen ModuleName parameter) as the actual folder. Defaults to c:\Scripts\lib.ps\Modules
+
+.PARAMETER ModuleName
+    The default will be the repo name (the end of the URL path). Can be over-ridden with this switch
+
+.PARAMETER ReadOnly
+    Only clones the current version (i.e., runs 'git clone --depth 1') so as to make a "non-publishable" copy of the repo
+
+.PARAMETER Force
+    Force the removal of any existing files for this module and redownload the files. Useful for updates to "read-only" copies
+
+.EXAMPLE
+    PS C:\> Get-GitModule.ps1 -url https://github.com/brsh/psSysInfo -Verbose -force -ReadOnly
+
+    Pulls down my psSysInfo module, over-writing all files, showing all messages
+
+.EXAMPLE
+    PS C:\> Get-GitModule.ps1 -url https://github.com/brsh/psSysInfo -force -ReadOnly -ModuleName "Dave"
+
+    Pulls down my psSysInfo module, over-writing all files, but calls the destination folder "Dave"
+
+.EXAMPLE
+    PS C:\> Get-GitModule.ps1 -url https://github.com/brsh/psSysInfo
+
+    Tries to create a standard clone, failing if the destination folder already exists
+#> 
+
+
+param (
+    [string] $url,
+    [string] $destPath = "C:\Scripts\lib.ps\Modules",
+    [Parameter(Mandatory=$false)]
+    [string] $ModuleName,
+    [switch] $ReadOnly = $false,
+    [switch] $force = $false
+)
+
+if (-not $ModuleName) {
+    $ModuleName = $url.Split("/")[-1]
+}
+
+$destPath = "${destpath}\${ModuleName}"
+
+if (test-path $destPath) {
+    if ($force) {
+        Write-Verbose "Removing $destpath ..."
+        try { 
+            Remove-Item $destPath -Force -Recurse -ErrorAction Stop
+        }
+        Catch {
+            Write-Warning "Unable to remove full directory structure"
+            "Error Message: {0}" -f $_.Exception.Message | Write-Warning
+            break
+        }
+    }
+    else {
+        Write-Verbose "Failed to clone $destPath"
+        Write-Warning "Path $destPath already exists"
+        Write-Warning "Use the -Force switch to overwrite"
+        break
+    }
+
+}
+
+if ($ReadOnly) {
+    if ($VerbosePreference -ne [System.Management.Automation.ActionPreference]::SilentlyContinue) {
+        git clone $url $destPath --depth 1
+    }
+    else {
+        git clone $url $destPath --depth 1 --quiet
+    }
+}
+else {
+    if ($VerbosePreference -ne [System.Management.Automation.ActionPreference]::SilentlyContinue) {
+        git clone $url $destpath
+    }
+    else {
+        git clone $url $destpath --quiet
+    }
+}
---
+++ readme.md
@@ -0,0 +1,38 @@
+My Powershell Library
+
+This sets up my PowerShell environment - including my profile and my "standard" scripts and modules. Of course, to load everything, I still need to manually doctor a main PowerShell Profile file. I use the AllUsersCurrentHost, cuz I'm every user (hmm... reminds me of a song). 
+
+My default folder location for scripting is, aptly, C:\Scripts. I keep this library, therefore, in C:\Scripts\libs.ps. For some degree of security, I adjust the permissions so that the Owner is "Administrator" from the local machine - then I remove Write permissions from any non-Administrator user/group. This forces a UAC prompt (or abject failure) if I try to modify anything without Admin elevation. If you're not familiar with NTFS and permissions, I recommend looking into them....
+
+So, to use this pick your profile file:
+
+name                   | path
+----                   | ----
+AllUsersAllHosts       | C:\Windows\System32\WindowsPowerShell\v1.0\profile.ps1
+AllUsersCurrentHost    | C:\Windows\System32\WindowsPowerShell\v1.0\Microsoft.PowerShell_profile.ps1
+CurrentUserAllHosts    | C:\Users\bshea\Documents\WindowsPowerShell\profile.ps1
+CurrentUserCurrentHost | C:\Users\bshea\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1
+
+and add the following:
+
+```
+if (test-path C:\Scripts\lib.ps\Profile\profile.ps1) {
+    . C:\Scripts\lib.ps\Profile\profile.ps1
+}
+```
+
+To Install:
+
+Download and extract the zip from: https://github.com/brsh/lib.ps/archive/master.zip
+
+To get my modules, after installing and loading the profile (either restart PowerShell or just dot source the profile.ps1), then run:
+
+```
+Get-GitModule.ps1 -url https://github.com/brsh/psSysInfo -force -ReadOnly
+Get-GitModule.ps1 -url https://github.com/brsh/psPrompt -force -ReadOnly
+Get-GitModule.ps1 -url https://github.com/brsh/psOutput -force -ReadOnly
+```
+
+
+
+
