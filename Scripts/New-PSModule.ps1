6fd868fdc619c08b56f5becd864e0097a4bdc354
Brian Sheaffer <brsh@users.noreply.github.com>
2016-09-07 10:46:48 -0700

    Added New-PSModule
    Added a script to create a skeleton for new modules - subfolders, the
    psm and psd files... the basics. Been toying with the idea for awhile,
    but ran into an example on github, so....

---
+++ Scripts/New-PSModule.ps1
@@ -0,0 +1,72 @@
+﻿<#
+.SYNOPSIS
+    Creates PowerShell module skeleton files (psm and psd)
+ 
+.DESCRIPTION
+    Creates a PowerShell module skeleton, complete with manifest and requisite folders (one for the module/manifest and the private subfolder) 
+
+.PARAMETER Name
+    Name of the script module
+
+.PARAMETER Path
+    Parent folder where it will create the script module; it must already exist.
+
+.PARAMETER Author
+    Specifies the author (defaults to AD Full Name)
+
+.PARAMETER Description
+    Description of the module
+
+.EXAMPLE
+     New-PSModule -Name Brontosaurus -Path "$env:ProgramFiles\WindowsPowerShell\Modules" -Author 'Anne Elk' -Description 'This is my module of brontosauruses'
+#>
+
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory=$true)]
+    [string]$Name,
+        
+    [ValidateScript({
+        If (Test-Path -Path $_ -PathType Container) {
+            $true
+        }
+        else {
+            Throw "'$_' is not a valid directory."
+        }
+    })]
+    [String]$Path = (($env:PSModulePath).ToString().Split(";") -like "*\Users\*"),
+
+    [Parameter(Mandatory=$false)]
+    [string]$Author,
+
+    [Parameter(Mandatory=$true)]
+    [string]$Description
+)
+
+$Copyright = 'To the extent within my power and possible under law, the author(s) have dedicated all copyright and related and neighboring rights to the public domain worldwide. This is distributed without any warranty.'
+
+New-Item -Path $Path -Name $Name -ItemType Directory | Out-Null
+New-Item -Path $Path\$Name -Name "private" -ItemType Directory | Out-Null
+
+Out-File -FilePath "$Path\$Name\$Name.psm1" -Encoding utf8
+
+$Template = @'
+#region Private Variables
+# Current script path
+[string]$ScriptPath = Split-Path (get-variable myinvocation -scope script).value.Mycommand.Definition -Parent
+#endregion Private Variables
+ 
+#region Private Helpers
+ 
+# Dot sourcing private script files
+Get-ChildItem $ScriptPath/private -Recurse -Filter "*.ps1" -File | Foreach { 
+    . $_.FullName
+}
+#endregion Load Private Helpers
+'@
+
+   
+Add-Content -Path "$Path\$Name\$Name.psm1" -Value $Template
+
+New-ModuleManifest -Path "$Path\$Name\$Name.psd1" -RootModule $Name -Author $Author -Description $Description `
+    -AliasesToExport $null -FunctionsToExport $null -VariablesToExport $null -CmdletsToExport $null -Copyright $Copyright
