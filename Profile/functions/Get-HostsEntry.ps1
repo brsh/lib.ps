3c15d4967b97573ce33579f33d4ea40b4b0deb4a
Brian Sheaffer <brsh@users.noreply.github.com>
2016-09-13 07:17:20 -0700

    Added get-hostsentry
    New function to read the hosts file.

---
+++ Profile/functions/Get-HostsEntry.ps1
@@ -0,0 +1,27 @@
+﻿function Get-HostsEntry {
+    $HostsFile = "$env:windir\System32\drivers\etc\hosts"
+
+    $results = get-content  $HostsFile | Where-Object { ((-not $_.StartsWith("#")) -and ($_.Trim().Length -gt 0)) }
+
+    $results | ForEach-Object {
+        [string] $ip, [string] $hostname, [string] $comment = $_.ToString().Trim() -split "\s+"
+
+        $InfoHash =  @{
+            IP = $ip
+            Computer = $hostname
+            Comment = $comment.TrimStart("#").Trim()
+        }
+        $InfoStack += New-Object -TypeName PSObject -Property $InfoHash
+
+        #Add a (hopefully) unique object type name
+        $InfoStack.PSTypeNames.Insert(0,"Hosts.Information")
+
+        #Sets the "default properties" when outputting the variable... but really for setting the order
+        $defaultProperties = @('Computer', 'IP', 'Comment')
+        $defaultDisplayPropertySet = New-Object System.Management.Automation.PSPropertySet(‘DefaultDisplayPropertySet’,[string[]]$defaultProperties)
+        $PSStandardMembers = [System.Management.Automation.PSMemberInfo[]]@($defaultDisplayPropertySet)
+        $InfoStack | Add-Member MemberSet PSStandardMembers $PSStandardMembers
+
+        $InfoStack
+    }
+}
