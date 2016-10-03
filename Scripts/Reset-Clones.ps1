60b414407f932fb0bc3802a7c551867b8f52e187
Brian Sheaffer <brsh@users.noreply.github.com>
2016-10-03 11:04:35 -0700

    Add clones subdir and script
    A new feature. Handy powershell repositories now go in the Clones
    folder. Similar to Modules, these scripts do not sync to the Lib.PS
    repo, but can be cloned and integrated into the lib.
    
    I also added the reset-clones.ps1 script to simplify the init (and
    re-init) of the library's clones!

---
+++ Scripts/Reset-Clones.ps1
@@ -0,0 +1,16 @@
+﻿## Initializes (or RE-initializes) the Lib.PS Modules and Clones
+## This is not intended to init the entire lib.ps .. 
+## just the items that live under lib.ps\Moducles and lib.ps\Clones
+
+if( ([System.Environment]::OSVersion.Version.Major -gt 5) -and ( # Vista and ...
+    new-object Security.Principal.WindowsPrincipal (
+       [Security.Principal.WindowsIdentity]::GetCurrent()) # current user is admin
+       ).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator) )
+{
+    get-content $LibPath\Settings\Get-Git-Clones.ini | get-GitModule.ps1 -Destination C:\Scripts\lib.ps\Clones\ -ReadOnly -Force
+    get-content $LibPath\Settings\Get-GitModule.ini | get-GitModule.ps1 -Destination C:\Scripts\lib.ps\Modules -ReadOnly -Force
+} else {
+    "Please run this script as an Administrator"
+}
+
+
