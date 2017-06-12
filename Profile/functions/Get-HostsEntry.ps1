9630488c1f3d4e47443693ae935c6857431ce358
Brian Sheaffer <brsh@users.noreply.github.com>
2017-06-12 15:00:13 -0700

    Revamped Get-HostsEntry
    Get-HostsEntry now works with multiple names for the same ip. Plus, I
    added search.

--- Profile/functions/Get-HostsEntry.ps1
+++ Profile/functions/Get-HostsEntry.ps1
@@ -1,12 +1,12 @@
 ﻿function Get-HostsEntry {
-<# 
-.SYNOPSIS 
+<#
+.SYNOPSIS
   Breaks the Hosts file in PS Objects
 
-.DESCRIPTION 
+.DESCRIPTION
     Just a simple script to parse the hosts file
 
-.EXAMPLE 
+.EXAMPLE
     Get-HostsEntry.ps1
 
     Outputs the entire hosts file with name, IP, and comment (if any)
@@ -15,32 +15,44 @@
     Get-HostsEntry.ps1 | Where-Object { $_.Computer -match "SRV" }
 
     Filters the list to names that contain SRV
-#> 
+#>
+
+	[CmdletBinding()]
+	param (
+		[Parameter(Mandatory=$false,Position=0)]
+		[Alias('Code', 'String', 'Text', 'Response')]
+		[string] $Search
+	)
 
     $HostsFile = "$env:windir\System32\drivers\etc\hosts"
 
-    $results = get-content  $HostsFile | Where-Object { ((-not $_.StartsWith("#")) -and ($_.Trim().Length -gt 0)) }
-
+    $results = get-content  $HostsFile | Where-Object { ((-not $_.Trim().StartsWith("#")) -and ($_.Trim().Length -gt 0)) }
+    if ($search) { $results = $results | Where-Object { $_ -match $search } }
     $results | ForEach-Object {
-        [string] $ip, [string] $hostname, [string] $comment = $_.ToString().Trim() -split "\s+"
+        [string] $line = $_.ToString().Trim()
 
-        $InfoHash =  @{
-            IP = $ip
-            Computer = $hostname
-            Comment = $comment.TrimStart("#").Trim()
+        [string] $ip = ($line -split "\s+")[0].Trim()
+        [string] $hostentries, [string] $comment = ($line -replace "^$ip").split("#")
+
+        foreach ($hostname in ($hostentries.Trim() -split "\s+")) {
+            $InfoHash =  @{
+                IP = $ip
+                Computer = $hostname.Trim()
+                Comment = $comment.Trim().TrimStart("#").Trim()
+            }
+            $InfoStack = New-Object -TypeName PSObject -Property $InfoHash
+
+            #Add a (hopefully) unique object type name
+            $InfoStack.PSTypeNames.Insert(0,"Hosts.Information")
+
+            #Sets the "default properties" when outputting the variable... but really for setting the order
+            $defaultProperties = @('Computer', 'IP', 'Comment')
+            $defaultDisplayPropertySet = New-Object System.Management.Automation.PSPropertySet(‘DefaultDisplayPropertySet’,[string[]]$defaultProperties)
+            $PSStandardMembers = [System.Management.Automation.PSMemberInfo[]]@($defaultDisplayPropertySet)
+            $InfoStack | Add-Member MemberSet PSStandardMembers $PSStandardMembers
+
+            $InfoStack
         }
-        $InfoStack = New-Object -TypeName PSObject -Property $InfoHash
-
-        #Add a (hopefully) unique object type name
-        $InfoStack.PSTypeNames.Insert(0,"Hosts.Information")
-
-        #Sets the "default properties" when outputting the variable... but really for setting the order
-        $defaultProperties = @('Computer', 'IP', 'Comment')
-        $defaultDisplayPropertySet = New-Object System.Management.Automation.PSPropertySet(‘DefaultDisplayPropertySet’,[string[]]$defaultProperties)
-        $PSStandardMembers = [System.Management.Automation.PSMemberInfo[]]@($defaultDisplayPropertySet)
-        $InfoStack | Add-Member MemberSet PSStandardMembers $PSStandardMembers
-
-        $InfoStack
     }
 }
 
