5ceb9eadbec1dbcf0433325ec1d06b30f9e6a0a1
Brian Sheaffer <brsh@users.noreply.github.com>
2017-07-25 14:27:19 -0700

    Create-LibPS should work...
    Of course, I haven't fully tested it (although I did overwrite my
    working lib.ps accidentally at one point, so that's something). It
    should be a lot closer to working than it was before anyway

--- Create-LibPS.ps1
+++ Create-LibPS.ps1
@@ -30,134 +30,196 @@
 
 [CmdletBinding(SupportsShouldProcess)]
 param (
-    [Parameter(Mandatory=$false)]
-    [string] $Folder = "C:\Scripts"
+	[Parameter(Mandatory = $false)]
+	[string] $Folder = "C:\Scripts"
 )
 
 #For Writing/Troubleshooting purposes (and because I forget to set it at runtime)
-$VerbosePreference="Continue"
+$VerbosePreference = "Continue"
 #$VerbosePreference="SilentlyContinue"
 
 Function Read-Profiles {
-    #Reload all profiles - helpful when editing/testing profiles
-    Set-Variable -name isDotSourced -value $False -Scope 0
-    $isDotSourced = $MyInvocation.InvocationName -eq '.' -or $MyInvocation.Line -eq ''
-    if (!($isDotSourced)) { write-host "You must dot source this function" -fore Red; write-host "`t. Load-Profiles`n`t. re-Profs"
-    -ForegroundColor "Yellow"; return "" }
-        @(
-            $Profile.AllUsersAllHosts,
-            $Profile.AllUsersCurrentHost,
-            $Profile.CurrentUserAllHosts,
-            $Profile.CurrentUserCurrentHost
-        ) | ForEach-Object {
-            if(Test-Path $_){
-                Write-Host "Loading $_"
-                . $_
-            }
-        }
+	#Reload all profiles - helpful when editing/testing profiles
+	Set-Variable -name isDotSourced -value $False -Scope 0
+	$isDotSourced = $MyInvocation.InvocationName -eq '.' -or $MyInvocation.Line -eq ''
+	if (!($isDotSourced)) {
+		write-host "You must dot source this function" -fore Red; write-host "`t. Load-Profiles`n`t. re-Profs"
+		-ForegroundColor "Yellow"; return ""
+	}
+	@(
+		$Profile.AllUsersAllHosts,
+		$Profile.AllUsersCurrentHost,
+		$Profile.CurrentUserAllHosts,
+		$Profile.CurrentUserCurrentHost
+	) | ForEach-Object {
+		if (Test-Path $_) {
+			Write-Host "Re-loading $_"
+			. $_
+		}
+	}
 }
 
 Function Remove-InvalidFileNameChars {
-    param(
-        [Parameter(Mandatory=$true, Position=0)]
-        [String] $Text
-    )
-    #Let's remove invalid filesystem characters and ones we just don't like in filenames
-    $invalidChars = ([IO.Path]::GetInvalidFileNameChars() + "," + ";") -join ''
-    $invalidChars = $invalidChars.Replace("\", "")
-    $invalidChars = $invalidChars.Replace(":", "")
-    $invalidChars = $invalidChars.Replace(" ", "")
-    $re = "[{0}]" -f [RegEx]::Escape($invalidChars)
-    $Text = $Text -replace $re
-    return $Text
+	param(
+		[Parameter(Mandatory = $true, Position = 0)]
+		[String] $Text
+	)
+	#Let's remove invalid filesystem characters and ones we just don't like in filenames
+	$invalidChars = ([IO.Path]::GetInvalidFileNameChars() + "," + ";") -join ''
+	$invalidChars = $invalidChars.Replace("\", "")
+	$invalidChars = $invalidChars.Replace(":", "")
+	$invalidChars = $invalidChars.Replace(" ", "")
+	$re = "[{0}]" -f [RegEx]::Escape($invalidChars)
+	$Text = $Text -replace $re
+	return $Text
 }
 
 $Folder = Remove-InvalidFileNameChars $Folder
 
 try {
-    [System.Management.Automation.PathInfo] $FolderInfo = Resolve-Path $Folder -ErrorAction Stop
-    [String] $HomeFolder = $FolderInfo.ProviderPath
-}
-catch { $HomeFolder = $Folder }
+	[System.Management.Automation.PathInfo] $FolderInfo = Resolve-Path $Folder -ErrorAction Stop
+	[String] $HomeFolder = $FolderInfo.ProviderPath
+} catch { $HomeFolder = $Folder }
 
 if (-not $(Test-Path $HomeFolder -ErrorAction SilentlyContinue)) {
-    $Title = "The Folder, $HomeFolder, does not exist. Create it?"
-    $Info = "Would you like to continue?"
+	$Title = "The Folder, $HomeFolder, does not exist. Create it?"
+	$Info = "Would you like to continue?"
 
-    $Options = [System.Management.Automation.Host.ChoiceDescription[]] @("&Yes", "&No")
-    [int] $DefaultChoice = 0
-    $Response =  $host.UI.PromptForChoice($Title , $Info, $Options, $DefaultChoice)
+	$Options = [System.Management.Automation.Host.ChoiceDescription[]] @("&Yes", "&No")
+	[int] $DefaultChoice = 0
+	$Response = $host.UI.PromptForChoice($Title , $Info, $Options, $DefaultChoice)
 
-    switch($Response) {
-        0 { try { $HomeFolder = (new-item $HomeFolder -ItemType "Directory" -ErrorAction Stop).FullName } catch { "Couldn't create path $HomeFolder"; "$_"; exit }; break }
-        1 { "Terminating script"; exit; break }
-    }
+	switch ($Response) {
+		0 { try { $HomeFolder = (new-item $HomeFolder -ItemType "Directory" -ErrorAction Stop).FullName } catch { "Couldn't create path $HomeFolder"; "$_"; exit }; break }
+		1 { "Terminating script"; exit; break }
+	}
 }
 
 Try {
-    if ($pscmdlet.ShouldProcess($HomeFolder, "Set-Location")) {
-        Set-Location $HomeFolder -ErrorAction Stop
-    }
+	if ($pscmdlet.ShouldProcess($HomeFolder, "Set-Location")) {
+		Set-Location $HomeFolder -ErrorAction Stop
+	}
+} catch {
+	"Couldn't change directory to the new location $HomeFolder. Terminating script."
+	exit
 }
-catch {
-    "Couldn't change directory to the new location $HomeFolder. Terminating script."
-    exit
- }
+
+"Testing if git is in the path"
+$git = (get-command git.exe -ErrorAction SilentlyContinue).Source
+if (-not ($git)) {
+	"Can't find git in path; trying GitHub's weird location..."
+	$git = (get-command (Resolve-Path "C:\Users\*\AppData\Local\GitHub\PortableGit_*\cmd\git.exe" | Select-Object -First 1).Path -ErrorAction SilentlyContinue).Source
+	if (-not ($git)) {
+		"Can't find git; terminating script"
+		exit
+	}
+}
+
+$pinfo = New-Object System.Diagnostics.ProcessStartInfo
+$pinfo.RedirectStandardError = $true
+$pinfo.RedirectStandardOutput = $true
+$pinfo.UseShellExecute = $false
+$pinfo.FileName = $git
+[string] $output = ""
 
 if ($pscmdlet.ShouldProcess("LibPS", "Cloning Repository")) {
-    "Testing if git is in the path"
-    $git = (get-command git.exe -ErrorAction SilentlyContinue).Source
-    if (-not ($git)) {
-        "Can't find git in path; trying GitHub's weird location..."
-        $git = (get-command (Resolve-Path "C:\Users\*\AppData\Local\GitHub\PortableGit_*\cmd\git.exe" | Select-Object -First 1).Path -ErrorAction SilentlyContinue).Source
-        if (-not ($git)) {
-            "Can't find git; terminating script"
-            exit
-        }
-    }
+	if (Test-Path $HomeFolder\Lib.PS -ErrorAction SilentlyContinue) {
+		$Title = "Lib.PS is already installed under $HomeFolder. This will overwrite and reset it!"
+		$Info = "Would you like to continue?"
 
-    "Cloning Main Lib.PS"
-    $retval = Start-Process $git -ArgumentList "clone https://github.com/brsh/lib.ps" -Wait -NoNewWindow
+		$Options = [System.Management.Automation.Host.ChoiceDescription[]] @("&Yes", "&No")
+		[int] $DefaultChoice = 1
+		$Response = $host.UI.PromptForChoice($Title , $Info, $Options, $DefaultChoice)
+
+		switch ($Response) {
+			0 {
+				try {
+					set-location $HomeFolder\lib.ps
+					[Environment]::CurrentDirectory = (Get-Location -PSProvider FileSystem).ProviderPath
+					"Starting git fetch..."
+					$pinfo.Arguments = "fetch --all"
+					$p = New-Object System.Diagnostics.Process
+					$p.StartInfo = $pinfo
+					$p.Start() | Out-Null
+					$p.WaitForExit()
+					$output = ""
+					$output = $p.StandardError.ReadToEnd()
+					if ($output -ne "") { throw "Error running 'git fetch --all'"}
+					#$retval = Start-Process $git -ArgumentList "fetch --all" -Wait -NoNewWindow
+					"Starting git reset..."
+					$pinfo.Arguments = "reset --hard origin/master"
+					$p = New-Object System.Diagnostics.Process
+					$p.StartInfo = $pinfo
+					$p.Start() | Out-Null
+					$p.WaitForExit()
+					$output = ""
+					$output = $p.StandardError.ReadToEnd()
+					if ($output -ne "") { throw "Error running 'git reset --hard origin/master'"}
+					#$retval = Start-Process $git -ArgumentList "reset --hard origin/master" -Wait -NoNewWindow
+					"Starting git pull..."
+					$pinfo.Arguments = "pull origin master"
+					$p = New-Object System.Diagnostics.Process
+					$p.StartInfo = $pinfo
+					$p.Start() | Out-Null
+					$p.WaitForExit()
+					$output = ""
+					$output = $p.StandardError.ReadToEnd()
+					#if ($output -ne "") { throw "Error running 'git pull origin master'"}
+					#(fricken 'up-to-date' repo 'error')
+					#$retval = Start-Process $git -ArgumentList "pull origin master" -Wait -NoNewWindow
+				} catch {
+					"Couldn't update/reset Lib.PS. Terminating script"
+					"Git error was: $($output)"
+					$_.Exception.Message
+					exit
+				} break
+			}
+			1 { "Terminating script"; exit; break }
+		}
+	} else {
+		"Cloning Main Lib.PS"
+		$retval = Start-Process $git -ArgumentList "clone https://github.com/brsh/lib.ps" -Wait -NoNewWindow
+	}
 }
 
 if ($pscmdlet.ShouldProcess($PROFILE, "Adjusting default profile")) {
-    try {
-        "Updating your personal profile: $profile"
-        $loadtext = @"
+	try {
+		"Updating your personal profile: $profile"
+		$loadtext = @"
 if (test-path $HomeFolder\lib.ps\Profile\profile.ps1) {
     . $HomeFolder\lib.ps\Profile\profile.ps1
 }
 "@
-        [bool] $DoUpdateProfile = $true
-        $profile | Get-Member *Host* | ForEach-Object { $_.name } |
-            ForEach-Object {
-                if (test-path $profile.$_) {
-                    if (Get-Content $profile.$_ | Where-Object { $_.Contains("lib.ps") }) {
-                        "Lib.ps exists in $profile.$_ - Skipping."
-                        $DoUpdateProfile = $false
-                    }
-                }
-            }
-        if ($DoUpdateProfile) {
-            if (-not (test-path $profile)) { $null = New-item –type file –force $profile }
-            $loadtext | Out-File -Append -FilePath $profile -Force
-        }
-    }
-    Catch {
-        "Couldn't update $profile"
-        $_
-        "You will need to adjust the profile manually."
-        "Once complete, you will want to run Reset-Clones.ps1 to bring in the rest of the library."
-        exit
-    }
+		[bool] $DoUpdateProfile = $true
+		$profile | Get-Member *Host* | ForEach-Object { $_.name } |
+			ForEach-Object {
+			if (test-path ($profile).$_) {
+				if (Get-Content ($profile).$_ | Where-Object { $_.Contains("lib.ps") }) {
+					"Lib.ps exists in ($profile).$_ - Skipping."
+					"You might want to adjust the profile manually by adjusting with the following:"
+					$loadtext
+					$DoUpdateProfile = $false
+				}
+			}
+		}
+		if ($DoUpdateProfile) {
+			if (-not (test-path $profile)) { $null = New-item –type file –force $profile }
+			$loadtext | Out-File -Append -FilePath $profile -Force
+		}
+	} Catch {
+		"Couldn't update $profile"
+		$_
+		"You will need to adjust the profile manually by adding the following:"
+		$loadtext
+	}
+	. Read-Profiles
 }
 
 if ($pscmdlet.ShouldProcess("Standard Modules/Scripts", "Cloning Sub-Repositories")) {
-    . Read-Profiles
-    if (test-path ".\lib.ps\Scripts\Reset-Clones.ps1") {
-        .\lib.ps\Scripts\Reset-Clones.ps1
-    }
-    Set-Location ..
+	if (test-path ".\lib.ps\Scripts\Reset-Clones.ps1") {
+		.\lib.ps\Scripts\Reset-Clones.ps1 -Folder $HomeFolder\lib.ps
+	}
+	Set-Location ..
 }
 
 
--- Scripts/Reset-Clones.ps1
+++ Scripts/Reset-Clones.ps1
@@ -1,54 +1,62 @@
-﻿## Initializes (or RE-initializes) the Lib.PS Modules and Clones
-## This is not intended to init the entire lib.ps .. 
+﻿
+## Initializes (or RE-initializes) the Lib.PS Modules and Clones
+## This is not intended to init the entire lib.ps ..
 ## just the items that live under lib.ps\Modules and lib.ps\Clones
 
-if (-not $LibPath) {
-    "The LibPath variable doesn't seem to be initialized."
-    "Is Lib.PS cloned/installed?"
-    return
+[CmdletBinding(SupportsShouldProcess)]
+param (
+	[Parameter(Mandatory = $false)]
+	[string] $Folder = $LibPath
+)
+
+if (-not $Folder) {
+	""
+	"Folder paramater not specified and the LibPath variable doesn't seem to be initialized."
+	"Is Lib.PS cloned/installed? Maybe you forgot the -Folder parameter??"
+	""
+	return
 }
 
-if( ([System.Environment]::OSVersion.Version.Major -gt 5) -and ( # Vista and ...
-    new-object Security.Principal.WindowsPrincipal (
-       [Security.Principal.WindowsIdentity]::GetCurrent()) # current user is admin
-       ).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator) )
-{
+if ($pscmdlet.ShouldProcess("LibPS", "Cloning Repository")) {
+	if ( ([System.Environment]::OSVersion.Version.Major -gt 5) -and ( # Vista and ...
+			new-object Security.Principal.WindowsPrincipal (
+				[Security.Principal.WindowsIdentity]::GetCurrent()) # current user is admin
+		).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator) ) {
 
-    if (-not (test-path $LibPath\Clones)) { 
-        
-        "Creating the Clones folder..." 
-        try {
-            mkdir "$LibPath\Clones" -ea stop | ForEach-Object { "   $_  -- Success" }
-        }
-        catch {
-            "Could not create the folder."
-            $_.Exception.Message
-            return
-        }
-    }
-    
-    if (-not (test-path $LibPath\Modules)) { 
-        "Creating the Modules folder..."
-        try {
-            mkdir "$LibPath\Modules" -ea stop | ForEach-Object { "   $_  -- Success" }
-        }
-        catch {
-            "Could not create the folder."
-            $_.Exception.Message
-            return
-        }
-    }
+		if (-not (test-path $Folder\Clones)) {
 
-    "Cloning the default scripts ($LibPath\Settings\Get-Git-Clones.ini):"
-    get-content $LibPath\Settings\Get-Git-Clones.ini | ForEach-Object { "    $_" }
-    get-content $LibPath\Settings\Get-Git-Clones.ini | get-GitModule.ps1 -Destination $LibPath\Clones -ReadOnly -Force -Verbose
-    
-    "Cloning the default Modules ($LibPath\Settings\Get-Git-Modules.ini):"
-    get-content $LibPath\Settings\Get-Git-Modules.ini | ForEach-Object { "    $_" }
-    get-content $LibPath\Settings\Get-Git-Modules.ini | get-GitModule.ps1 -Destination $LibPath\Modules -ReadOnly -Force -Verbose
+			"Creating the Clones folder..."
+			try {
+				mkdir "$Folder\Clones" -ea stop | ForEach-Object { "   $_  -- Success" }
+			} catch {
+				"Could not create the folder."
+				$_.Exception.Message
+				return
+			}
+		}
 
-} else {
-    "Please run this script as an Administrator"
+		if (-not (test-path $Folder\Modules)) {
+			"Creating the Modules folder..."
+			try {
+				mkdir "$Folder\Modules" -ea stop | ForEach-Object { "   $_  -- Success" }
+			} catch {
+				"Could not create the folder."
+				$_.Exception.Message
+				return
+			}
+		}
+
+		"Cloning the default scripts ($Folder\Settings\Get-Git-Clones.ini):"
+		get-content $Folder\Settings\Get-Git-Clones.ini | ForEach-Object { "    $_" }
+		get-content $Folder\Settings\Get-Git-Clones.ini | get-GitModule.ps1 -Destination $Folder\Clones -ReadOnly -Force -Verbose
+
+		"Cloning the default Modules ($Folder\Settings\Get-Git-Modules.ini):"
+		get-content $Folder\Settings\Get-Git-Modules.ini | ForEach-Object { "    $_" }
+		get-content $Folder\Settings\Get-Git-Modules.ini | get-GitModule.ps1 -Destination $Folder\Modules -ReadOnly -Force -Verbose
+
+	} else {
+		"Please run this script as an Administrator"
+	}
+
+
 }
-
-
