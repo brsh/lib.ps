5961e4a4ccebbcdc0d6ab297b538dfbfb5c1de7f
Brian Sheaffer <brsh@users.noreply.github.com>
2019-03-05 14:34:42 -0800

    Enhanced start-sleepcounter - now with objects, acceleration, & aborts!

--- Scripts/Start-SleepCounter.ps1
+++ Scripts/Start-SleepCounter.ps1
@@ -22,6 +22,21 @@ How many seconds between counts. Default is 1 second
 .PARAMETER Milliseconds
 How many milliseconds between counts (mutually exclusive with seconds)
 
+.PARAMETER Up
+Count Up rather than the default down
+
+.PARAMETER NoAbort
+Do not allow Control-C to abort the countdown
+
+.PARAMETER NoSpeedup
+Don't allow keys pressed to speed up the countdown (i.e., skip numbers)
+
+.PARAMETER ReturnObject
+Returns a boolean value if the script was completed (true) or aborted (false)
+
+.Outputs
+If -ReturnObject is set, it returns a Boolean whether the script was completed naturally or aborted
+
 .EXAMPLE
 Start-SleepCounter.ps1
 
@@ -32,53 +47,91 @@ Start-SleepCounter.ps1 -StatusMessage 'Taking a break for 30 seconds...' -Durati
 
 Taking a break for 30 seconds... Back Now!
 
+.EXAMPLE
+Start-SleepCounter.ps1 -StatusMessage 'Taking a break for 30 seconds...' -Duration 30 -Seconds 1 -CompletionMessage 'Back Now!' -ReturnObject
+
+Taking a break for 30 seconds... Aborted
+False
+
 #>
 
 
-[CmdletBinding(DefaultParameterSetName='Seconds')]
+[CmdletBinding(DefaultParameterSetName = 'Seconds')]
 param (
-    [Parameter(Mandatory=$false)]
-    [Alias('Status', 'Text')]
-    [string] $StatusMessage = 'Please wait...',
-    [Parameter(Mandatory=$false)]
-    [Alias('Done')]
-    [string] $CompletionMessage = 'Done!',
-    [Parameter(Mandatory=$false)]
-    [Alias('HowLong', 'Delay', 'Count')]
-    [int] $Duration = 10,
-    [Parameter(Mandatory=$false,ParameterSetName='Seconds')]
-    [int] $Seconds = 1,
-    [Parameter(Mandatory=$false,ParameterSetName='MilliSeconds')]
-    [int] $Milliseconds = 0,
-    [Switch] $Up = $false
+	[Parameter(Mandatory = $false)]
+	[Alias('Status', 'Text')]
+	[string] $StatusMessage = 'Please wait...',
+	[Parameter(Mandatory = $false)]
+	[Alias('Done')]
+	[string] $CompletionMessage = 'Done!',
+	[Parameter(Mandatory = $false)]
+	[Alias('HowLong', 'Delay', 'Count')]
+	[int] $Duration = 10,
+	[Parameter(Mandatory = $false, ParameterSetName = 'Seconds')]
+	[int] $Seconds = 1,
+	[Parameter(Mandatory = $false, ParameterSetName = 'MilliSeconds')]
+	[int] $Milliseconds = 0,
+	[Switch] $Up = $false,
+	[Switch] $NoAbort = $false,
+	[Switch] $NoSpeedup = $false,
+	[Switch] $ReturnObject = $false
 )
 
+[bool] $CurrentControlCAsInput = [console]::TreatControlCAsInput
+[console]::TreatControlCAsInput = $true
+
 function Write-StatusLine {
-    param ($count)
-    Write-Host "`r" -NoNewline
-    Write-Host $StatusMessage -NoNewline -ForegroundColor Green
-    Write-Host -NoNewLine "$count".PadLeft($Durationlength) -ForegroundColor yellow
-    if ($Milliseconds -gt 0) { Start-Sleep -Milliseconds $Milliseconds } else { Start-Sleep -Seconds $Seconds }
+	param ($count)
+	Write-Host "`r" -NoNewline
+	Write-Host $StatusMessage -NoNewline -ForegroundColor Green
+	Write-Host -NoNewLine "$count".PadLeft($Durationlength) -ForegroundColor yellow
 }
 
 #Let's keep the message all on one line (not that you'd ever send too long a line ...)
 [int] $Durationlength = $Duration.ToString().Length + 2
 [int] $screenwidth = $Host.UI.RawUI.WindowSize.Width - $Durationlength
 if ($StatusMessage.Length -gt $screenwidth) {
-    [int] $CharsToCut = $screenwidth - $Durationlength - 3
-    $StatusMessage = $StatusMessage.Substring(0, $CharsToCut) + "..."
+	[int] $CharsToCut = $screenwidth - $Durationlength - 3
+	$StatusMessage = $StatusMessage.Substring(0, $CharsToCut) + "..."
 }
 if ($StatusMessage.Length -gt 0) { $CompletionMessage = " " + $CompletionMessage }
+[ConsoleColor] $Color = 'Green'
 
-if ($Up) {
-    for ($a=1; $a -le $Duration; $a++) {
-        Write-StatusLine -count $a
-    }
-} else {
-    for ($a=$Duration; $a -ge 1; $a--) {
-        Write-StatusLine -count $a
-    }
+[int] $b = $Duration + 1
+
+if ($Up) { $b = 0 }
+
+for ($a = $Duration; $a -ge 1; $a--) {
+	if ($Up) {
+		$b++
+	} else {
+		$b--
+	}
+	Write-StatusLine -count $b
+	if ([console]::KeyAvailable) {
+		$x = [System.Console]::ReadKey($true)
+
+		if (($x.modifiers -band [consolemodifiers]"control") -and ($x.key -eq "C") -and (-not $NoAbort)) {
+			$CompletionMessage = " Aborted"
+			$Color = 'Red'
+			$a = 0
+		} else {
+			$SleepIt = $NoSpeedup
+		}
+		$Host.UI.RawUI.FlushInputBuffer()
+	} else {
+		$SleepIt = $true
+	}
+	if ($SleepIt) {
+		if ($Milliseconds -gt 0) { Start-Sleep -Milliseconds $Milliseconds } else { Start-Sleep -Seconds $Seconds }
+	}
 }
 Write-Host "`r" -NoNewline
 Write-Host $StatusMessage -NoNewline -ForegroundColor White
-Write-Host $CompletionMessage.PadRight($Durationlength) -ForegroundColor Green
+Write-Host $CompletionMessage.PadRight($Durationlength) -ForegroundColor $Color
+
+[console]::TreatControlCAsInput = $CurrentControlCAsInput
+
+if ($ReturnObject) {
+	if ($CompletionMessage -match 'Aborted') { $false } else { $true }
+}
