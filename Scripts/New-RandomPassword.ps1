7cfba6d1ef988d07ee5b418fb8cbec15a6a6ab81
brsh <brsh@users.noreply.github.com>
2020-02-18 08:29:47 -0800

    new-randompassword pipeline support and obj output

--- Scripts/New-RandomPassword.ps1
+++ Scripts/New-RandomPassword.ps1
@@ -5,10 +5,26 @@ Generate a random password
 .DESCRIPTION
 Generate a random password of a specific length (default is 25 characters).
 
-You can specify your own characters to use via the -Characters switch.
+You can specify your own characters to use via the Characters switch.
 Otherwise, the script uses the following:
 
     '!#$%&*+-0123456789?@ABCDEFGHIJKLMNOPQRSTUVWXYZ^_abcdefghijklmnopqrstuvwxyz~'
+
+Note: Some of those chars might not be "web safe"
+
+The script will take pipeline input for all the parameters. You can feed it objects
+from a CSV file or some other method of choice. Length and UserName can be specified
+by name or by datatype (integer and string, respectively); Characters has to be
+explicitly defined.
+
+.PARAMETER Length
+How many characters to generate
+
+.PARAMETER Characters
+Specify the characters to pick from (handy if some of the standard chars aren't "safe")
+
+.PARAMETER UserName
+A username field in case you gen a bunch of passwords
 
 .EXAMPLE
 New-RandomPassword.ps1
@@ -25,16 +41,39 @@ New-RandomPassword -Characters 'abcdefABCDEF1234'
 
 cCE2A2Daf4fDb2EfbeDe
 
+.EXAMPLE
+$a = @()
+$a += [pscustomobject] @{ length = 10; UserName = 'user1'; Characters = 'aAbBcCdD12345'}
+$a += [pscustomobject] @{ length = 10; UserName = 'user2'}
+$a += [pscustomobject] @{ length = 30; UserName = 'user3'}
+$a | New-RandomPassword.ps1
+
 #>
-
+[CmdLetBinding()]
 param (
-	[int] $length = 25,
-	[string] $Characters = '!#$%&*+-0123456789?@ABCDEFGHIJKLMNOPQRSTUVWXYZ^_abcdefghijklmnopqrstuvwxyz~'
+	[Parameter(Mandatory = $false, ValueFromPipelineByPropertyName = $true, ValueFromPipeline = $true)]
+	[int] $Length = 25,
+	[Parameter(Mandatory = $false, ValueFromPipelineByPropertyName = $true, ValueFromPipeline = $false)]
+	[string] $Characters = '!#$%&*+-0123456789?@ABCDEFGHIJKLMNOPQRSTUVWXYZ^_abcdefghijklmnopqrstuvwxyz~',
+	[Parameter(Mandatory = $false, ValueFromPipelineByPropertyName = $true, ValueFromPipeline = $true)]
+	[string] $UserName = ''
 )
-[string[]] $ascii = $Characters.ToCharArray() | Sort-Object -unique
+BEGIN { }
 
-For ($loop = 1; $loop -le $length; $loop++) {
-	$TempPassword += ($ascii | GET-RANDOM)
+PROCESS {
+	[string[]] $ascii = $Characters.ToCharArray() | Sort-Object -unique
+	[string] $TempPassword = ''
+
+	For ($loop = 1; $loop -le $length; $loop++) {
+		$TempPassword += ($ascii | GET-RANDOM)
+	}
+	$hash = [ordered] @{
+		Password = $TempPassword
+	}
+	if ($UserName.Length -gt 0) {
+		$hash.UserName = $UserName
+	}
+	New-Object -TypeName PSCustomObject -Property $hash
 }
-$TempPassword
 
+END { }
