0721247ce99870a06d1cd07f9a66b3f0fe823b6b
Brian Sheaffer <brsh@users.noreply.github.com>
2020-08-29 11:10:24 -0700

    Added Dad Jokes

---
+++ Scripts/Get-DadJoke.ps1
@@ -0,0 +1,39 @@
+<#
+.SYNOPSIS
+Uses the icanhazdadjoke.com api to .. get a dad joke
+
+.DESCRIPTION
+Sometimes, you just need a dad joke. Whether to remind you of your own father
+or just cuz to remember a time before you were all groan up #dadjoke
+
+.PARAMETER Search
+A string to search for
+
+.EXAMPLE
+get-dadjoke
+Returns a dad joke
+
+.EXAMPLE
+get-dadjoke -search 'hipster'
+
+Returns any dad jokes with the word hipster
+
+.LINK
+http://icanhazdadjoke.com
+#>
+param (
+	[string] $Search = ''
+)
+$URI = 'icanhazdadjoke.com'
+
+if ($Search.Trim().Length -gt 0) {
+	[void][System.Reflection.Assembly]::LoadWithPartialName("System.web")
+	$URI = "$URI/search?term=$([System.Web.HttpUtility]::UrlEncode($Search))"
+	write-host $URI
+}
+
+$headers = @{
+	'accept'     = 'text/plain'
+	'User-Agent' = 'PowerShell Script for Fun (https://github.com/brsh/lib.ps) - thanks for the chuckles'
+}
+(Invoke-WebRequest -UseBasicParsing -Uri $URI -Headers $Headers).Content
